fd8cabed5f9070f6dad1fc15eb513185d804581f
Randy <randybancey@gmail.com>
2026-09-04 16:17:57 -0400

    Remove unnecessary assets

---
+++ src/test_game/load_asset.py
@@ -0,0 +1,21 @@
+"""
+This module loads assets from a set of spritesheets.
+"""
+
+from pathlib import Path
+import arcade
+
+spritesheet_list: dict[str, list[Path, Path]] = {
+    "background_elements" : [
+        Path()
+    ],
+    "hexagon-building" : [
+
+    ],
+    "hexagon-tiles" : [
+
+    ],
+    "ui-pack-rpg-expansion" : [
+
+    ]
+}
